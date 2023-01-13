edb4d6e7015edfd136ee085328dd6756f6e10db5
YD1RUH <32731955+YD1RUH@users.noreply.github.com>
2023-01-14 05:24:31 +0700

    FIX bug while transmit file large than 10 KB

--- LISTENER.sh
+++ LISTENER.sh
@@ -6,6 +6,7 @@ iter=$(cat raw/data)
 echo "$iter" > raw/un.txt
 iconv -f us-ascii -t utf-8 raw/un.txt -c -o raw/output.txt
 cat raw/output.txt
+echo "" > raw/rawdata
 
 c=0
 file=1
@@ -16,14 +17,16 @@ do
     rec audio/"$file.wav" rate 8k silence 1 0.1 3% 1 3.0 3%
     ./decode raw/"$file.raw" audio/"$file.wav"
     iconv -f us-ascii -t utf-8 raw/"$file.raw" -c -o raw/"$file.craw"
+    cat raw/"$file.craw" >> raw/rawdata
+    rm raw/"$file.raw" raw/"$file.craw"
     sleep 1
     a=$[ a-1 ]
     file=$[ file+1 ]
 done
 cd raw
-cat *.craw > rawdata
-cat rawdata | base64 --decode > result.pdf
-mv result.pdf ../
+#cat *.craw > rawdata
+cat rawdata | base64 --decode > result.PNG
+mv result.PNG ../
 rm * .
 cd ..
 echo "finish.."
